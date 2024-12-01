e56623faafa0e7e101388936f692950be891343c
jthro <jethro.rosette@outlook.com>
2024-12-01 12:37:15 +1100

    add functionality to connect to / create a room

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,21 @@ import "./App.css";
 import Grid from "./components/Grid";
 import { TileType } from "./components/Tile/types";
 import useGrid from "./hooks/useGrid";
+import * as Colyseus from "colyseus.js";
+import { BACKEND_URL } from './backend.ts';
+
+enum FEState {
+  ENTER_GAME,
+  ENTER_NAME,
+  IN_GAME,
+  END
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  console.log(BACKEND_URL);
+  const [state, setState] = useState(FEState.ENTER_GAME);
   const [grid, updateGrid] = useGrid();
+  const [gameCode, updateGameCode] = useState('');
 
   const play = (index: number) => {
     if (index < 0 || index > 8 || grid[index] !== TileType.Empty) return;
@@ -17,6 +28,15 @@ function App() {
     newGrid[index] = TileType.X;
     updateGrid(newGrid);
   };
+
+
+  const client = new Colyseus.Client(BACKEND_URL);
+
+  const attemptJoinGame = async (game: string): Promise<string> => {
+    const room = await client.joinOrCreate(game);
+    console.log(room.sessionId, "joined", room.name);
+    return room.sessionId;
+  }
 
   const [inGame, setInGame] = useState(false);
 
@@ -30,25 +50,33 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button onClick={() => setInGame(!inGame)}>
+      <h1>3ᴺoughts & Crosses</h1>
+            <button onClick={() => setInGame(!inGame)}>
         {inGame ? "Leave" : "Start"} Game
       </button>
       {inGame && (
         <div className="game">
           <Grid tiles={grid} onPlay={(i) => play(i)} />
         </div>
+      )}
+      {state === FEState.ENTER_GAME && (
+        <form>
+          <label>Game Code:</label>
+          <input
+            type="text"
+            onChange={(e) => updateGameCode(e.target.value)}
+          />
+        </form>
+      )}
+      {state === FEState.ENTER_GAME && (
+        <button onClick={() => {
+          try {
+            attemptJoinGame(gameCode);
+            setState(FEState.ENTER_NAME);
+          } catch (e) {
+            alert(e);
+          };
+        }}/>
       )}
     </>
   );
